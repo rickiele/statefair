c48526f199bb6cc1355ea7412bcc9b4dc5d356b3
rickiele <rickienthefishies@gmail.com>
2020-12-09 13:18:24 -0600

    initial commit

---
+++ scripts/TicketBooth.js
@@ -0,0 +1,13 @@
+const contentTarget = document.querySelector(".entry")
+const eventHub = document.querySelector("#state-fair")
+
+eventHub.addEventListener()
+
+export const TicketBooth = () => {
+    contentTarget.innerHTML = `
+        <div class="ticketBooth">
+          <button id="rideTicket">Ride Ticket</button>
+        </div>
+    `
+}
+
